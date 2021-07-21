25f842eb0abb65c483c1d4572c3b60fed1108f9f
Bruno Souza <bruno.arubesu@gmail.com>
2021-07-21 19:25:22 -0300

    chore: add permission hook

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,28 @@
 import { useContext, useEffect } from "react"
+import Router from 'next/router';
 import { AuthContext } from "../contexts/AuthContext"
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth"
+import { useCan } from "../hooks/useCan";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, isAuthenticated } = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  });
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response))
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 }
 
